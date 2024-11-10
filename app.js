41c18a9c5708f173136bf09ac1500a70ea52f227
Obaddd <obadaljabberi@outlook.com>
2024-11-10 14:38:19 -0500

    Updated Submit Button
    Made the submit semi functional

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +20,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongoose.connect('mongodb+srv://obadaljabberi:obad1234@cluster0.mgss7.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const ItemSchema = new mongoose.Schema({
+//   name: String,
+//   description: String
+// });
+
+// const Item = mongoose.model('Item', ItemSchema);
+
+// app.post('/create', async (req, res) => {
+//   const { name, description } = req.body;
+
+//   try {
+//     const newItem = new Item({ name, description });
+//     await newItem.save();
+//     res.redirect('/');
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("An error occured while saving the data.");
+//   }
+// });
+
+app.post('/create', (req, res) => {
+  const { name, description } = req.body;
+  res.redirect('/');
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
